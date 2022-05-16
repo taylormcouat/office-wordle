71df1fbc7fd0ac6096972e1f672b29bfd148d53a
taylormcouat <70711386+taylormcouat@users.noreply.github.com>
2022-05-16 14:18:26 -0700

    adding dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import SelectSearch from 'react-select-search';
+import { menuItems } from "./menuItems"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style = {{ display: 'flext', alignItems: 'center', justifyContent: 'center' }}>
+      <SelectSearch options={menuItems} placeholder="Search" />
     </div>
   );
 }
---
+++ src/menuItems.js
@@ -0,0 +1,779 @@
+export const menuItems = [
+    {
+      name: 'Season 1',
+      type: 'group',
+      items: [{
+        name: 'Pilot',
+        value: 1
+      },
+      {
+        name: 'Diversity Day',
+        value: 2
+      },
+      {
+        name: 'Health Care',
+        value: 3
+      },
+      {
+        name: 'The Alliance',
+        value: 4
+      },
+      {
+        name: 'Basketball',
+        value: 5
+      },
+      {
+        name: 'Hot Girl',
+        value: 6
+      }
+    ]
+    },
+    {
+      name: 'Season 2',
+      type: 'group',
+      items: [{
+        name: 'The Dundies',
+        value: 7
+      },
+      {
+        name: 'Sexual Harassment',
+        value: 8
+      },
+      {
+        name: 'Office Olympics',
+        value: 9
+      },
+      {
+        name: 'The Fire',
+        value: 10
+      },
+      {
+        name: 'Halloween',
+        value: 11
+      },
+      {
+        name: 'The Fight',
+        value: 12
+      },
+      {
+        name: 'The Client',
+        value: 13
+      },
+      {
+        name: 'Performance Review',
+        value: 14
+      },
+      {
+        name: 'E-Mail Surveillance',
+        value: 15
+      },
+      {
+        name: 'Christmas Party',
+        value: 16
+      },
+      {
+        name: 'Booze Cruise',
+        value: 17
+      },
+      {
+        name: 'The Injury',
+        value: 18
+      },
+      {
+        name: 'The Secret',
+        value: 19
+      },
+      {
+        name: 'The Carpet',
+        value: 20
+      },
+      {
+        name: 'Boys and Girls',
+        value: 21
+      },
+      {
+        name: "Valentine's Day",
+        value: 22
+      },
+      {
+        name: "Dwight's Speech",
+        value: 23
+      },
+      {
+        name: "Take Your Daughter to Work Day",
+        value: 24
+      },
+      {
+        name: "Michael's Birthday",
+        value: 25
+      },
+      {
+        name: "Drug Testing",
+        value: 26
+      },
+      {
+        name: "Conflict Resolution",
+        value: 27
+      },
+      {
+        name: "Casino Night",
+        value: 28
+      }
+    ]},
+    {
+        name: 'Season 3',
+        type: 'group',
+        items: [{
+          name: 'Gay Witch Hunt',
+          value: 29
+        },
+        {
+          name: 'The Convention',
+          value: 30
+        },
+        {
+          name: 'The Coup',
+          value: 31
+        },
+        {
+          name: 'Grief Counseling',
+          value: 32
+        },
+        {
+          name: 'Initiation',
+          value: 33
+        },
+        {
+          name: 'Diwali',
+          value: 34
+        },
+        {
+          name: 'Branch Closing',
+          value: 35
+        },
+        {
+          name: 'The Merger',
+          value: 36
+        },
+        {
+          name: 'The Convict',
+          value: 37
+        },
+        {
+          name: 'A Benihana Christmas',
+          value: 38
+        },
+        {
+          name: 'Back From Vacation',
+          value: 39
+        },
+        {
+          name: 'Traveling Salesmen',
+          value: 40
+        },
+        {
+          name: 'Ben Franklin',
+          value: 41
+        },
+        {
+          name: "Phyllis' Wedding",
+          value: 42
+        },
+        {
+          name: 'Business School',
+          value: 43
+        },
+        {
+          name: 'Cocktails',
+          value: 44
+        },
+        {
+          name: "The Negotiation",
+          value: 45
+        },
+        {
+          name: "Safety Training",
+          value: 46
+        },
+        {
+          name: "Product Recall",
+          value: 47
+        },
+        {
+          name: "Women's Appreciation",
+          value: 48
+        },
+        {
+          name: 'Beach Games',
+          value: 49
+        },
+        {
+          name: "The Job",
+          value: 50
+        }
+      ]},
+      {
+        name: 'Season 4',
+        type: 'group',
+        items: [{
+          name: 'Fun Run',
+          value: 51
+        },
+        {
+          name: 'Dunder Mifflin Infinity',
+          value: 52
+        },
+        {
+          name: 'Launch Party',
+          value: 53
+        },
+        {
+          name: 'Money',
+          value: 54
+        },
+        {
+          name: 'Local Ad',
+          value: 55
+        },
+        {
+          name: 'Branch Wars',
+          value: 56
+        },
+        {
+          name: 'Survivor Man',
+          value: 57
+        },
+        {
+          name: 'The Deposition',
+          value: 58
+        },
+        {
+          name: 'Dinner Party',
+          value: 59
+        },
+        {
+          name: 'Chair Model',
+          value: 60
+        },
+        {
+          name: 'Night Out',
+          value: 61
+        },
+        {
+          name: 'Did I Stutter?',
+          value: 62
+        },
+        {
+          name: 'Job Fair',
+          value: 63
+        }
+      ]},
+      {
+        name: 'Season 5',
+        type: 'group',
+        items: [{
+          name: 'Weight Loss',
+          value: 64
+        },
+        {
+          name: 'Business Ethics',
+          value: 65
+        },
+        {
+          name: 'Baby Shower',
+          value: 66
+        },
+        {
+          name: 'Crime Aid',
+          value: 67
+        },
+        {
+          name: 'Employee Transfer',
+          value: 68
+        },
+        {
+          name: 'Customer Survey',
+          value: 69
+        },
+        {
+          name: 'Business Trip',
+          value: 70
+        },
+        {
+          name: 'Frame Toby',
+          value: 71
+        },
+        {
+          name: 'The Surplus',
+          value: 72
+        },
+        {
+          name: 'Moroccan Christmas',
+          value: 73
+        },
+        {
+          name: 'The Duel',
+          value: 74
+        },
+        {
+          name: 'Prince Family Paper',
+          value: 75
+        },
+        {
+          name: 'Stress Relief',
+          value: 76
+        },
+        {
+          name: "Lecture Circuit: Part 1",
+          value: 77
+        },
+        {
+          name: 'Lecture Circuit: Part 2',
+          value: 78
+        },
+        {
+          name: 'Blood Drive',
+          value: 79
+        },
+        {
+          name: "Golden Ticket",
+          value: 80
+        },
+        {
+          name: "New Boss",
+          value: 81
+        },
+        {
+          name: "Two Weeks",
+          value: 82
+        },
+        {
+          name: "Michael Scott Paper Company",
+          value: 83
+        },
+        {
+          name: 'Heavy Competition',
+          value: 84
+        },
+        {
+          name: "Broke",
+          value: 85
+        },
+        {
+          name: "Casual Friday",
+          value: 86
+        },
+        {
+          name: 'Cafe Disco',
+          value: 87
+        },
+        {
+          name: "Company Picnic",
+          value: 88
+        }
+      ]},
+      {
+        name: 'Season 6',
+        type: 'group',
+        items: [{
+          name: 'Gossip',
+          value: 89
+        },
+        {
+          name: 'The Meeting',
+          value: 90
+        },
+        {
+          name: 'The Promotion',
+          value: 91
+        },
+        {
+          name: 'Niagra: Part 1',
+          value: 92
+        },
+        {
+          name: 'Niagra: Part 2',
+          value: 93
+        },
+        {
+          name: 'Mafia',
+          value: 94
+        },
+        {
+          name: 'The Lover',
+          value: 95
+        },
+        {
+          name: 'Koi Pond',
+          value: 96
+        },
+        {
+          name: 'Double Date',
+          value: 97
+        },
+        {
+          name: 'Murder',
+          value: 98
+        },
+        {
+          name: 'Shareholder Meeting',
+          value: 99
+        },
+        {
+          name: "Scott's Tots",
+          value: 100
+        },
+        {
+          name: 'Secret Santa',
+          value: 101
+        },
+        {
+          name: "The Banker",
+          value: 102
+        },
+        {
+          name: 'Sabre',
+          value: 103
+        },
+        {
+          name: 'Manager and Salesman',
+          value: 104
+        },
+        {
+          name: "The Delivery: Part 1",
+          value: 105
+        },
+        {
+          name: "The Delivery: Part 2",
+          value: 106
+        },
+        {
+          name: "St. Patrick's Day",
+          value: 107
+        },
+        {
+          name: "New Leads",
+          value: 108
+        },
+        {
+          name: 'Happy Hour',
+          value: 109
+        },
+        {
+          name: "Secretary's Day",
+          value: 110
+        },
+        {
+          name: "Body Language",
+          value: 111
+        },
+        {
+          name: 'The Cover-Up',
+          value: 112
+        },
+        {
+          name: "The Chump",
+          value: 113
+        },
+        {
+          name: "Whistleblower",
+          value: 114
+        }
+      ]},
+      {
+        name: 'Season 7',
+        type: 'group',
+        items: [{
+          name: 'Nepotism',
+          value: 115
+        },
+        {
+          name: 'Counseling',
+          value: 116
+        },
+        {
+          name: "Andy's Play",
+          value: 117
+        },
+        {
+          name: 'Sex Ed',
+          value: 118
+        },
+        {
+          name: 'The Sting',
+          value: 119
+        },
+        {
+          name: 'Costume Contest',
+          value: 120
+        },
+        {
+          name: 'Christening',
+          value: 121
+        },
+        {
+          name: 'Viewing Party',
+          value: 122
+        },
+        {
+          name: 'WUPHF.com',
+          value: 123
+        },
+        {
+          name: 'China',
+          value: 124
+        },
+        {
+          name: 'Classy Christmas',
+          value: 125
+        },
+        {
+          name: "Ultimatum",
+          value: 126
+        },
+        {
+          name: 'The Seminar',
+          value: 127
+        },
+        {
+          name: "The Search",
+          value: 128
+        },
+        {
+          name: 'PDA',
+          value: 129
+        },
+        {
+          name: 'Threat Level Midnight',
+          value: 130
+        },
+        {
+          name: "Todd Packer",
+          value: 131
+        },
+        {
+          name: "Garage Sale",
+          value: 132
+        },
+        {
+          name: "Training Day",
+          value: 133
+        },
+        {
+          name: "Michael's Last Dundies",
+          value: 134
+        },
+        {
+          name: 'Goodbye, Michael',
+          value: 135
+        },
+        {
+          name: "The Inner Circle",
+          value: 136
+        },
+        {
+          name: "Dwight K. Schrute, (Acting) Manager",
+          value: 137
+        },
+        {
+          name: 'Search Committee',
+          value: 138
+        }
+      ]},
+      {
+        name: 'Season 8',
+        type: 'group',
+        items: [{
+          name: 'The List',
+          value: 139
+        },
+        {
+          name: 'The Incentive',
+          value: 140
+        },
+        {
+          name: "Lotto",
+          value: 141
+        },
+        {
+          name: 'Garden Party',
+          value: 142
+        },
+        {
+          name: 'Spooked',
+          value: 143
+        },
+        {
+          name: 'Doomsday',
+          value: 144
+        },
+        {
+          name: "Pam's Replacement",
+          value: 145
+        },
+        {
+          name: 'Gettysburg',
+          value: 146
+        },
+        {
+          name: 'Mrs. California',
+          value: 147
+        },
+        {
+          name: 'Christmas Wishes',
+          value: 148
+        },
+        {
+          name: 'Trivia',
+          value: 149
+        },
+        {
+          name: "Pool Party",
+          value: 150
+        },
+        {
+          name: 'Jury Duty',
+          value: 151
+        },
+        {
+          name: "Special Project",
+          value: 152
+        },
+        {
+          name: 'Tallahassee',
+          value: 153
+        },
+        {
+          name: 'After Hours',
+          value: 154
+        },
+        {
+          name: "Test the Store",
+          value: 155
+        },
+        {
+          name: "Last Day in Florida",
+          value: 156
+        },
+        {
+          name: "Get the Girl",
+          value: 157
+        },
+        {
+          name: "Welcome Party",
+          value: 158
+        },
+        {
+          name: 'Angry Andy',
+          value: 159
+        },
+        {
+          name: "Fundraiser",
+          value: 160
+        },
+        {
+          name: "Turf War",
+          value: 161
+        },
+        {
+          name: 'Free Family Portrait Studio',
+          value: 162
+        }
+      ]},
+      {
+        name: 'Season 9',
+        type: 'group',
+        items: [{
+          name: 'New Guys',
+          value: 163
+        },
+        {
+          name: "Roy's Wedding",
+          value: 164
+        },
+        {
+          name: "Andy's Ancestry",
+          value: 165
+        },
+        {
+          name: 'Work Bus',
+          value: 166
+        },
+        {
+          name: 'Here Comes Treble',
+          value: 167
+        },
+        {
+          name: 'The Boat',
+          value: 168
+        },
+        {
+          name: "The Whale",
+          value: 169
+        },
+        {
+          name: 'The Target',
+          value: 170
+        },
+        {
+          name: 'Dwight Christmas',
+          value: 171
+        },
+        {
+          name: 'Lice',
+          value: 172
+        },
+        {
+          name: 'Suit Warehouse',
+          value: 173
+        },
+        {
+          name: "Customer Loyalty",
+          value: 174
+        },
+        {
+          name: 'Junior Salesman',
+          value: 175
+        },
+        {
+          name: "Vandalism",
+          value: 176
+        },
+        {
+          name: 'Couples Discount',
+          value: 177
+        },
+        {
+          name: 'Moving On',
+          value: 178
+        },
+        {
+          name: "The Farm",
+          value: 179
+        },
+        {
+          name: "Promos",
+          value: 180
+        },
+        {
+          name: "Stairmageddon",
+          value: 181
+        },
+        {
+          name: "Paper Airplane",
+          value: 182
+        },
+        {
+          name: "Livin' The Dream",
+          value: 183
+        },
+        {
+          name: "A.A.R.M",
+          value: 184
+        },
+        {
+          name: "Finale",
+          value: 185
+        }
+      ]}
+]
